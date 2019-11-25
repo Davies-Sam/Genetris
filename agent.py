42d13cb6ebd7ff424b4fb35058780adb2001d677
Davies-Sam <49299059+Davies-Sam@users.noreply.github.com>
2019-11-25 15:47:14 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,6 @@
 """This file holds code for our AI agent its behavior#
 the agent is handed the board, the current and next pieces,
 weights, and the game object.
-using this infomration the agent will try every possible rotation
+using this information the agent will try every possible rotation
 at each column and will choose the column and rotation that yields
-the higheest score using the weightings. We must consider the moving into the
-last colum with each possible rotation as well, due to situational rotation lock at the edge columns"""
+the higheest score using the weightings. We wil rotate and sweep over the columns to find the best rotation/column combination"""

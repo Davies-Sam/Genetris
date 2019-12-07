bd4a968bdbac121b4b20c11a492b2ad710c55428
Sam <davies.sam1994@gmail.com>
2019-12-07 14:30:36 -0500

    staging

--- agent.py
+++ agent.py
@@ -4,3 +4,5 @@ weights, and the game object.
 using this information the agent will try every possible rotation
 at each column and will choose the column and rotation that yields
 the higheest score using the weightings. We wil rotate and sweep over the columns to find the best rotation/column combination"""
+#the agent will need duplicated board(s) to play on before sending the correct move back to the tetris app
+
--- heuristics.py
+++ heuristics.py
@@ -1,1 +1,11 @@
 #This file holds functions that will calculate the scores for our heuristics#
+
+"""
+We will start by using only 4 Heuristics
+1)Aggregate height
+2)Bumpiness (jaggedness or lack of flatness)
+3)Holes Created
+4)Lines Cleared
+"""
+
+
